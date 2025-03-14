26f5022bb7ca12db52e0b2239e63599f9fb3027e
vilajini003 <vilajini003@gmail.com>
2025-03-14 22:54:42 +0530

    uplode files

---
+++ layouts/admin_menu.php
@@ -0,0 +1,61 @@
+<ul>
+  <li>
+    <a href="admin.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-user"></i>
+      <span>User Management</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="group.php">Manage Groups</a> </li>
+      <li><a href="users.php">Manage Users</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="categorie.php" >
+      <i class="glyphicon glyphicon-indent-left"></i>
+      <span>Auction Item Type</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Bidding Items</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Manage Item</a> </li>
+       <li><a href="add_product.php">Add Item</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="media.php" >
+      <i class="glyphicon glyphicon-picture"></i>
+      <span>Media Files</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-credit-card"></i>
+       <span>Transection</span>
+      </a>
+      <ul class="nav submenu">
+         <li><a href="sales.php">Manage Billing</a> </li>
+         <li><a href="add_sale.php">Add Bills</a> </li>
+     </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-duplicate"></i>
+       <span>Auction Report</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="sales_report.php">Billing by dates </a></li>
+        <li><a href="monthly_sales.php">Monthly Bills</a></li>
+        <li><a href="daily_sales.php">Daily Bills</a> </li>
+      </ul>
+  </li>
+</ul>
---
+++ layouts/special_menu.php
@@ -0,0 +1,18 @@
+<ul>
+  <li>
+    <a href="home.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Auction Item Type</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Manage product</a> </li>
+       <li><a href="add_product.php">Add product</a> </li>
+   </ul>
+  </li>
+</ul>
